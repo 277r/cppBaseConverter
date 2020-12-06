b491658e6bbedbbc4e513e698a0f6543ec981f65
277r <rick_degraaf@hotmail.nl>
2020-12-06 15:53:30 +0100

    updated charHex to be reversible

--- charToHex.cpp
+++ charToHex.cpp
@@ -1,3 +1,4 @@
+// file should've been called charHex, because it'll be bidirectional
 #include <string>
 
 char data16[] = "0123456789ABCDEF";
@@ -22,3 +23,24 @@ std::string toHex(unsigned char input)
 	return returndata;
 }
 
+int ints16[] = {0, 1, 2, 3, 4, 5, 6,7, 8, 9, 10, 11, 12, 13, 14,15};
+int getInt16(char x)
+{
+	x -= '0';
+	for (int i = 0; i < sizeof(ints16) / 4; i++)
+	{
+		if (x == ints16[i]){
+			return ints16[i];
+		}
+	}
+	return 255;
+}
+
+// the strings accepted are only 2 bytes, because there's only 2 bytes (in (one extended ASCII character or byte) saved as 2 bytes in hex)
+char toChar(std::string input){
+	int b = input[0];
+	int a = input[1];
+	b = getInt16(b);
+	a = getInt16(a);
+	return b * 16 + a;
+}
